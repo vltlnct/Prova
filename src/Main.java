2e38708e6eb07fdc38522275762d7c30ef0acda6
ValterLanciotti <valter.lanciotti@digitazon.school>
2022-04-04 17:17:40 +0200

    primo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Ciao a tutti. Di nuovo");
+        System.out.println("Ciao a tutti. Di nuovo  ");
     }
 }
